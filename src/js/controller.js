ee8d6cbaf56ef0ebfa78ba9b95ee7ffd55dc50db
AnanM8 <anan.muthiah@gmail.com>
2022-06-02 20:31:21 +0800

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -113,6 +113,10 @@ const controlAddRecipe = async function (newRecipe) {
   location.reload();
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -121,6 +125,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
